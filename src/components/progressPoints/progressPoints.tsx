13607693c822e43d6ca08cdbb047590de926e571
unknown <saba4inasaridze@gmail.com>
2022-08-11 10:49:12 +0400

    Added progress logic

--- src/components/progressPoints/progressPoints.tsx
+++ src/components/progressPoints/progressPoints.tsx
@@ -2,20 +2,44 @@ import React from "react";
 import Image from "next/image";
 import atrinityLogo from "../../../public/assets/pictures/LOGO.jpg";
 
-const ProgressPoints = (props: { className?: string; stepsCount?: number }) => {
-  const { className, stepsCount } = props;
+const ProgressPoints = (props: {
+  className?: string;
+  totalSteps: number;
+  currStep: number;
+}) => {
+  const { className, totalSteps, currStep } = props;
+
+  const step = (
+    <div className="step">
+      <Image className="step" src={atrinityLogo} alt="atrLOGO" />
+    </div>
+  );
+
+  const steps = Array(totalSteps);
+  // console.log(totalSteps);
 
   return (
     <div className={className}>
-      <div className="step">
+      {/* <div className="step">
+        <Image className="step" src={atrinityLogo} alt="atrLOGO" />
+      </div>
+      <div className="step ">
         <Image className="step" src={atrinityLogo} alt="atrLOGO" />
       </div>
       <div className="step still">
         <Image className="step" src={atrinityLogo} alt="atrLOGO" />
-      </div>
-      <div className="step still">
-        <Image className="step" src={atrinityLogo} alt="atrLOGO" />
-      </div>
+      </div> */}
+      {[...Array(totalSteps)].map((_, idx) =>
+        idx < currStep + 1 ? (
+          <div key={idx} className="step">
+            <Image className="step" src={atrinityLogo} alt="atrLOGO" />
+          </div>
+        ) : (
+          <div key={idx} className="step">
+            <Image className="step still" src={atrinityLogo} alt="atrLOGO" />
+          </div>
+        )
+      )}
     </div>
   );
 };
