7a41c0eb5e05e486bde2c01c4f60109d316f2b6c
unknown <saba4inasaridze@gmail.com>
2022-08-12 13:46:06 +0400

    Implemented first version of technologies section, then I'll make it independent component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,17 @@ import elephant from "../../public/assets/svgs/e1.svg";
 import elephant1 from "../../public/assets/svgs/e2.svg";
 import elephant2 from "../../public/assets/svgs/e3.svg";
 import elephant3 from "../../public/assets/svgs/e4.svg";
+import htmlLogo from "../../public/assets/technologies/html.png";
+import cssLogo from "../../public/assets/technologies/css.png";
+import jsLogo from "../../public/assets/technologies/js.png";
+import tsLogo from "../../public/assets/technologies/TS.png";
+import reactLogo from "../../public/assets/technologies/react.png";
+import nextLogo from "../../public/assets/technologies/next.png";
+import reduxLogo from "../../public/assets/technologies/redux.svg";
+import scLogo from "../../public/assets/technologies/sc.png";
+import twLogo from "../../public/assets/technologies/tailwind.png";
+import qlLogo from "../../public/assets/technologies/graphql.png";
+import gitLogo from "../../public/assets/technologies/Git_icon.svg.png";
 import eFull from "../../public/assets/svgs/e4.svg";
 import etester from "../../public/assets/svgs/etester.svg";
 
@@ -63,7 +74,46 @@ const Home: NextPage = () => {
 
       <div className="sections">
         {/* <Image className="bg" src={etester} alt="el" /> */}
-        <section className="techs"></section>
+        <section className="techs">
+          <div className="techTitle">
+            <h1>Technologies I use</h1>
+          </div>
+          <div className="content">
+            <div className="tech">
+              <Image src={htmlLogo} alt="tech" />
+            </div>
+            <div className="tech">
+              <Image src={cssLogo} alt="tech" />
+            </div>
+            <div className="tech">
+              <Image src={jsLogo} alt="tech" />
+            </div>
+            <div className="tech">
+              <Image src={tsLogo} alt="tech" />
+            </div>
+            <div className="tech">
+              <Image src={reactLogo} alt="tech" />
+            </div>
+            <div className="tech">
+              <Image src={nextLogo} alt="tech" />
+            </div>
+            <div className="tech">
+              <Image src={reduxLogo} alt="tech" />
+            </div>
+            <div className="tech">
+              <Image src={scLogo} alt="tech" />
+            </div>
+            <div className="tech">
+              <Image src={twLogo} alt="tech" />
+            </div>
+            <div className="tech">
+              <Image src={qlLogo} alt="tech" />
+            </div>
+            <div className="tech">
+              <Image src={gitLogo} alt="tech" />
+            </div>
+          </div>
+        </section>
 
         <section className="projects"></section>
 
