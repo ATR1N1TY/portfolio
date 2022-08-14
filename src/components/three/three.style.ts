9d5b64738786c8e4e0a3da5f89af1da27b11c30c
unknown <saba4inasaridze@gmail.com>
2022-08-14 11:35:16 +0400

    Added div wrapper to canvas

--- src/components/three/three.style.ts
+++ src/components/three/three.style.ts
@@ -2,9 +2,10 @@ import styled from "styled-components";
 import Three from "./three";
 
 export const StyledThree = styled(Three)`
-  & {
-    width: 100%;
-    height: 100%;
-  }
-  /* position: absolute; */
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -9;
 `;
